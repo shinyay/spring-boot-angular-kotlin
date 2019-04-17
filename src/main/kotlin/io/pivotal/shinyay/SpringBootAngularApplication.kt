f30727f77fab9a4ad70b3e32243e866e3b447e3f
shinyay <shinya.com@gmail.com>
2019-04-17 23:06:59 +0900

    Add CommandLineRunner process for adding sample data

--- src/main/kotlin/io/pivotal/shinyay/SpringBootAngularApplication.kt
+++ src/main/kotlin/io/pivotal/shinyay/SpringBootAngularApplication.kt
@@ -1,11 +1,26 @@
 package io.pivotal.shinyay
 
+import io.pivotal.shinyay.entity.UserEntity
+import io.pivotal.shinyay.repository.UserRepository
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
 
 @SpringBootApplication
-class SpringBootAngularApplication
+class SpringBootAngularApplication {
+
+	@Bean
+	fun init(userRepository: UserRepository) = CommandLineRunner {
+		arrayOf("A-san", "B-san", "C-san", "D-san", "E-san").forEach { name ->
+			userRepository.save(UserEntity(name, "$name@pivotal.io"))
+		}
+	}
+}
 
 fun main(args: Array<String>) {
 	runApplication<SpringBootAngularApplication>(*args)
 }
+
+
+
