df9e60cf7926ed8d841661330a7ead27a69a6c2b
shinyay <shinya.com@gmail.com>
2019-04-17 23:05:02 +0900

    Add Entity annotation
    Modify variable order

--- src/main/kotlin/io/pivotal/shinyay/entity/UserEntity.kt
+++ src/main/kotlin/io/pivotal/shinyay/entity/UserEntity.kt
@@ -1,12 +1,14 @@
 package io.pivotal.shinyay.entity
 
+import javax.persistence.Entity
 import javax.persistence.GeneratedValue
 import javax.persistence.GenerationType
 import javax.persistence.Id
 
+@Entity
 data class UserEntity(
+        val name: String,
+        val email: String,
         @Id
         @GeneratedValue(strategy = GenerationType.AUTO)
-        val id: Long,
-        val name: String,
-        val email: String)
+        val id: Long = -1)
